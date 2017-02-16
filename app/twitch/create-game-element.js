2efa6bd9797a749b7932c754d19906854abe4d72
John Michael McCasland <jmmccasland@gmail.com>
2017-02-16 16:04:31 -0600

    01-Create Messages passes

--- app/twitch/create-game-element.js
+++ app/twitch/create-game-element.js
@@ -1,1 +1,10 @@
 export default null;
+// import data from './data.json';
+
+// IMG SWAP
+const gameImg = document.querySelector('.game__img');
+gameImg.src = data.top[0].game.box.large;
+
+// TITLE SWAP
+const gameTitle = document.querySelector('.game__title');
+gameTitle.innerText = 'CONNECTED, WOO WOO';
